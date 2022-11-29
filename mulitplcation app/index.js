1cdd8ae8112c057062d07702f7afdd915aab671a
jamsco-nln <jamyre.scott@nextlevelnola.org>
2022-11-29 22:36:36 +0000

    Finished app

--- mulitplcation app/index.js
+++ mulitplcation app/index.js
@@ -1,6 +1,38 @@
 const num1 = Math.ceil(Math.random() * 10);
 const num2 = Math.ceil(Math.random() * 10);
-const questionE1 = document.getElementById("question");
+const questionEl = document.getElementById("question");
 
-questionE1.innertext = `What is `
+const inputEl = document.getElementById("input");
 
+const formEl = document.getElementById("form");
+
+const scoreEl = document.getElementById("score");
+
+let score = JSON.parse(localStorage.getItem("score"));
+
+if(!score){
+    score = 0;
+}
+
+scoreEl.innerText =`score: ${score}`
+
+questionEl.innerText = `What is ${num1} mulitply by ${num2}?`
+
+const correctAns = num1 * num2;
+
+formEl.addEventListener("submit" , ()=>{
+    const userAns = +inputEl.value;
+    console.log(userAns, typeof userAns);
+    if(userAns === correctAns) {
+        score++;
+        updateLocalStorage();
+    } else {
+        score--;
+        updateLocalStorage();
+    }
+})
+
+function updateLocalStorage(){
+    localStorage.setItem("score", JSON.stringify(score));
+
+}
